6a41adda04a182f6826619b52eef5aff92b2f124
amir-pasalic <amirpasalic144@gmail.com>
2021-03-10 18:22:04 +0100

    added ex10

---
+++ ex10/src/index.js
@@ -0,0 +1,20 @@
+function main (numOne, numTwo) {
+
+    var product ;
+    var numOne =15;
+    var numTwo =3;
+    
+    product = numOne * numTwo
+    
+    
+
+return product ;
+    
+
+
+}
+
+
+console.log(main(15,3));
+
+module.export = main;
