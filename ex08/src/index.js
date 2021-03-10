509a489e6bb4bb70b41207defbef542ba70502dd
amir-pasalic <amirpasalic144@gmail.com>
2021-03-10 18:04:00 +0100

    added ex08

---
+++ ex08/src/index.js
@@ -0,0 +1,20 @@
+function main (numOne, numTwo) {
+
+    var sum ;
+    var numOne =15;
+    var numTwo =8;
+    
+    sum = numOne + numTwo
+    
+    
+
+return sum ;
+    
+
+
+}
+
+
+console.log(main(15,8));
+
+module.export = main;
