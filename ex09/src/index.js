651b33067b3f82e61df9c6e0aa7e7b8e9867b4ec
amir-pasalic <amirpasalic144@gmail.com>
2021-03-10 18:16:17 +0100

    added ex09

---
+++ ex09/src/index.js
@@ -0,0 +1,20 @@
+function main (numOne, numTwo) {
+
+    var difference ;
+    var numOne =15;
+    var numTwo =8;
+    
+    difference  = numOne - numTwo
+    
+    
+
+return difference ;
+    
+
+
+}
+
+
+console.log(main(15,8));
+
+module.export = main;
